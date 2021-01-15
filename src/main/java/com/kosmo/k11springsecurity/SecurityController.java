1d4d5f60fad9319e450f0613d2cc40ce75054c02
pinocchio9702 <pinocchio9702@gmail.com>
2021-01-15 14:34:52 +0900

    K11SpringSecurity_02

--- src/main/java/com/kosmo/k11springsecurity/SecurityController.java
+++ src/main/java/com/kosmo/k11springsecurity/SecurityController.java
@@ -3,13 +3,17 @@ package com.kosmo.k11springsecurity;
 import java.security.Principal;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SecurityController {
-	
+	/*
+	시크리티 1단계 : 기본페이지 사용하기 
+	 */
 	//USER권한이 있어야 접속가능한 요청명
 	@RequestMapping("/security1-1/index.do")
 	public String security1_1() {
@@ -22,14 +26,60 @@ public class SecurityController {
 		return "09Security/Step1/access";
 	}
 	
+	
+	/*
+	시큐리티 2단계 : 로그인 페이지 커스터마이징
+	 */
 	@RequestMapping("/security2/index.do")
 	public String securityIndex2(Principal principal,
 			Authentication authentication, Model model) {
+		/*
+		controller에서 시큐리티를 통해 로그인 한 사용자정보 얻어오기
+			:@Controller로 선언된 클래스에서는 메소드 인자로
+			principal 객체를 통해 사용자 아이디를 얻어올 수 있다.
+		 */
+		
+		//1. Principla 객체를 통한 아이디 얻기
+		String user_id = principal.getName();
+		System.out.println("user_id="+user_id );
+		
+		//2.Authentication객체를 통한 사용자 아이디 얻기
+		UserDetails userDetails =
+				(UserDetails)authentication.getPrincipal();
+		String detail_id = userDetails.getUsername();
+		System.out.println("detail_id="+detail_id);
+		
+		/*
+		일반적인 클래스에서 사용자 정보 얻어오기
+			: 스프링 컨테이너의 전역변수로 선언된 SecurityContextHolder
+			객체를 이용해서 사용자 아이디를 얻어올 수 있다.
+		 */
+		Object object = SecurityContextHolder.getContext()
+							.getAuthentication().getPrincipal();
+		UserDetails sch = (UserDetails)object;
+		String sch_id = sch.getUsername();
+		System.out.println("sch_id="+sch_id);
+		
+		model.addAttribute("user_id", user_id);
+		
 		return "08Security/Step2/index";
 	}
 	
 	@RequestMapping("/security2/login.do")
 	public String securityIndex2Login(Principal principal, Model model) {
+		
+		
+		String user_id="";
+		try {
+			user_id = principal.getName();
+			System.out.println("user_id="+user_id);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		model.addAttribute("user_id", user_id);
+		
+		
 		return "08Security/Step2/login";
 	}
 	
@@ -42,4 +92,31 @@ public class SecurityController {
 	public String securityIndex2AdminMain() {
 		return "08Security/Step2/adminMain";
 	}
+	
+	
+	@RequestMapping("/security3/form.do")
+	public String formPage() {
+		return "08Security/Warning/postForm";
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -6,6 +6,62 @@
 <head>
 <title>Home</title>
 </head>
+<!-- 
+Spring Security 사용절차
+
+1. 의존설정 - pom.xml에 시큐리티를 위한 의존설정을 한다.
+	
+	<dependency>
+		<groupId>org.springframework.security</groupId>
+		<artifactId>spring-security-web</artifactId>
+		<version>4.2.1.RELEASE</version>
+	</dependency>
+	<dependency>
+		<groupId>org.springframework.security</groupId>
+		<artifactId>spring-security-config</artifactId>
+		<version>4.2.1.RELEASE</version>
+	</dependency>
+	<dependency>
+		<groupId>org.springframework.security</groupId>
+		<artifactId>spring-security-taglibs</artifactId>
+		<version>4.2.1.RELEASE</version>
+	</dependency>
+	
+2. 시큐리티 설정파일의 경로와 파일명 지정 : web.xml
+	아래와 같이 컨텍스트 초기화 파라미터로  설정파일의 위치를 지정한다.
+
+	<context-param>
+		<param-name>contextConfigLocation</param-name>
+		<param-value>
+			/WEB-INF/spring/root-context.xml
+			/WEB-INF/spring/security-context3.xml
+		</param-value>
+	</context-param>
+	
+
+3.위에서 지정한 위치에 스프링 시큐리티 설정파일을 추가한다.
+	-파일 생성시 네임스페이스를 추가한다. (security)
+	-intercept-url과 같은 관련 속성은 해당 파일을 참조한다.
+	-security: 와 같은 접두어를 제거하고 싶을때는 3번 파일을 참조하여
+		네임스페이스 부분을 조금 수정한다.
+
+4. 시큐리티 적용을 위한 필터 설정을 한다. : web.xml
+	해당 설정을 통해 프로젝트의 모든 요청에 대해 시큐리티가 적용된다.
+	필터가 없으면 시큐리티는 적용되지 않는다.
+	<filter>
+		<filter-name>springSecurityFilterChain</filter-name>
+		<filter-class>org.springframework.web.filter.DelegatingFilterProxy
+		</filter-class>
+	</filter>
+	<filter-mapping>
+		<filter-name>springSecurityFilterChain</filter-name>
+		<url-pattern>/*</url-pattern>
+	</filter-mapping>
+
+5. 4번까지의 과정을 통해 시큐리티는 사용가능하고, 로그인페이지에 대한 
+	커스터마이징이나 JDBC연동은 설정파일 2, 3을 참조한다.
+	
+ -->
 <body>
 	<h2>Spring Security 사용1 - Default 페이지 사용</h2>
 	<li><a href="./security1-1/index.do" target="_blank"> Step1
@@ -23,6 +79,10 @@
 			관리자영역 바로가기 </a></li>
 	<li><a href="./security2/accessDenied.do" target="_blank">
 			Step2 접근불가안내페이지 바로가기 </a></li>
+	
+	<h2>Spring Security 사용시 주의사항</h2>
+	<li><a href="./security3/form.do" target="_blank">
+			Form페이지 바로가기 </a></li>
 
 </body>
 </html>
